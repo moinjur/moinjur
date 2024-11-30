ba84ee3be9146198541e723e27e6b312ca71ebc7
demo1 <690986500@qq.com>
2024-11-30 11:41:27 +0800

    feat: Complete development environment setup
    - Add logs page implementation
    - Add utils with time formatting
    - Add README.md with project documentation
    - Add VSCode settings
    - Setup complete development environment
    
    By: AI Programmer Mo

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error',
+  },
+};
---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      });
+    }
+  },
+  getUserProfile(e) {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        });
+      }
+    });
+  }
+});
